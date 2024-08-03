fedea30af6daa9a72c57ec76c805eb2cf97513b6
mehalimes <alymeshe05@gmail.com>
2024-08-03 15:59:33 +0300

    Solution Based Load Balancing With Nginx

---
+++ server1.solution/project1.project/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project1.project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getValue")]
+        public ActionResult getValue()
+        {
+            return Ok("Load Balancing to First Solution");
+        }
+    }
+}
---
+++ server2.solution/project2.project/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project2.project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getValue")]
+        public ActionResult getValue()
+        {
+            return Ok("Load Balancing to Second Solution");
+        }
+    }
+}
